3d80125423fe3955f31f72f0d5f3311da15ac441
shah.paritosh <shah.paritosh@238f7194-8534-0410-8b31-fb56ab3e27be>
2007-08-31 11:28:56 +0000

    basic watchlist view done.

--- trunk/watchlist_editor/watchlist_store.rb
+++ trunk/watchlist_editor/watchlist_store.rb
@@ -3,17 +3,32 @@ require 'gtk2'
 
 class WatchListModel < Gtk::ListStore
 
+	COLUMNS = [
+		[0, "Stock", String, "stock.symbol"],
+		[1, "Target Price", Float, "tgtprice"],
+		[2, "Last Trade", Float, "stock.quote.lastTrade"],
+		[3, "Change", Float, "stock.quote.changePercent"],
+		[4, "Spread", Float, "spread"]
+	]
+
 	def initialize
-		super(String)
+		params = []
+		COLUMNS.each do |colid, colname, coltype, accessor|
+			params << coltype
+		end
+		super(*params)
 	end
 	
 	# populate the model with watchlist at filepath
 	def populate(filepath)
 		watchlist = Watchlist.new
 		watchlist.import(filepath)
+		watchlist.fill_quotes
 		watchlist.each_key do |symbol|
 			iter = self.append
-			iter[0] = symbol
+			COLUMNS.each do |colid, colname, coltype, accessor|
+				iter[colid] = eval "watchlist[symbol]."+accessor
+			end
 		end
 	end
 
